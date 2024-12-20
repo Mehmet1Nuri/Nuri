7d017c8a62a117123a07e760c7dc591b80565307
Meowmet <74294784+meowmet@users.noreply.github.com>
2024-12-21 00:38:24 +0300

    Update spamner.py

--- spamner.py
+++ spamner.py
@@ -2,31 +2,16 @@ import pyautogui
 import time
 
 # Kullanıcıya hazırlanması için zaman ver
-print("2 saniye içinde metin girişi başlayacak. Hedef pencereye odaklanın.")
+print("5 saniye içinde metin girişi başlayacak. Hedef pencereye odaklanın.")
 time.sleep(5)
 
 # Girilecek metin
 text = "WOIIIII SANTUYYY "
 spammnumber = 500000
 
-
 for i in range(spammnumber):
     pyautogui.typewrite(text)
     pyautogui.press('enter')
-    time.sleep(0)  # Her girişten sonra kısa bir bekleme
+    time.sleep(0)  
 
 print("Metin girişi tamamlandı.")
-
-
-
-# import pyautogui
-# import time
-
-
-# time.sleep(5)
-
-# for i in range(2029):
-#     pyautogui.typewrite("woaaaw")
-#     pyautogui.hotkey('enter')
-#     # pyautogui.press('tab')
-#     time.sleep(0)  # Her girişten sonra kısa bir bekleme
